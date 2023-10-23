4e19612cfd8b0ed5c7a74a5695eb38d81e2100ec
justbru <j.brunings2020@gmail.com>
2023-10-22 20:08:00 -0700

    base logic for both parts

--- lab3/lab3_part1.c
+++ lab3/lab3_part1.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+// Declarations
+
 struct motor_command
 {
     uint8_t left_speed;
@@ -14,7 +16,26 @@ struct motor_command
 struct motor_command
 compute_proportional(uint8_t left, uint8_t right);
 
+// makes use of compute_proprtional below
 int main(void)
 {
     return 0;
+}
+
+
+/*
+    The function should take 2 uint8_t values as parameters (these will represent 2 sensor readings).  
+    The function should return a struct motor_command containing the speeds to assign to left and right motors.  
+    Verify that your proportional controller can run the track smoothly and without any jittery motion.
+*/
+motor_command compute_proportional(uint8_t left, uint8_t right)
+{
+    struct motor_command result_command;
+
+    leftSensor = analog(4);
+    rightSensor = analog(5) - 5;
+
+    // finalize the logic in oval.c before doing this
+
+    return result_command;
 }
--- lab3/lab3_part2.c
+++ lab3/lab3_part2.c
@@ -16,5 +16,80 @@ compute_proportional(uint8_t left, uint8_t right);
 
 int main(void)
 {
+    struct motor_command motor_values;
+    uint8_t leftSensor;
+    uint8_t rightSensor;
+
+    // immediately begin running the proportional controller
+    clear_display();
+    print_string("Proportional");
+
+    // Drive in proportional controller mode until button is pressed
+    while (!get_btn())
+    {
+        leftSensor = analog(0);
+        rightSensor = analog(1);
+        motor_values = compute_proportional(leftSensor, rightSensor);
+        motor(0, motor_values.left_speed);
+        motor(1, motor_values.right_speed);
+    }
+
+    // this will need to be in while loop
+    clear_display();
+    print_string("Data");
+
+    // Set cursor to second line and print sensor readings before printing training
+
+    while (!get_btn())
+    {
+        leftSensor = analog(0);
+        rightSensor = analog(1);
+        motor_values = compute_proportional(leftSensor, rightSensor);
+
+        // add motor_values to some array of values to later be used for training
+    }
+
+    // During this mode, you will use the captured training data along with the proportional controller to train the neural network
+    clear_display();
+    print_string("Training");
+
+    while (!get_btn())
+    {
+        // for data in dataSet
+
+        // add motor_values to some array of values to later be used for training
+    }
+
+    // run neural network
+    clear_display();
+    print_string("AI MODE");
+
+    while (!get_btn())
+    {
+        leftSensor = analog(0);
+        rightSensor = analog(1);
+        motor_values = compute_neural_network(leftSensor, rightSensor);
+        motor(0, motor_values.left_speed);
+        motor(1, motor_values.right_speed);
+    }
+
     return 0;
+}
+
+motor_command compute_proportional(uint8_t left, uint8_t right)
+{
+    struct motor_command result;
+
+    return result;
+}
+
+motor_command compute_neural_network(uint8_t left, uint8_t right)
+{
+    struct motor_command result;
+
+    return result;
+}
+
+void train_neural_network(){
+    // this will happen monday
 }
